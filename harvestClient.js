eb05d043c474dc793c515c70b0d04229113f882e
Sarah Crooke <sarahcrooke@Sarahs-MBP-2.lan>
2018-04-09 23:10:41 +1000

    add big query

--- harvestClient.js
+++ harvestClient.js
@@ -1,8 +1,15 @@
 var config = require('./config');
-var mysql      = require('mysql');
-var connection = mysql.createConnection(config.databaseOptions);
 var request = require('request');
+const json2csv = require('json2csv');
+var fs = require('fs');
+const BigQuery = require('@google-cloud/bigquery');
 //request information
+const bigquery = new BigQuery({
+  projectId: 'bigq-drd-1',
+  keyFilename: 'service-account-file.json'
+});
+const dataset = bigquery.dataset('Timesheets');
+const table = dataset.table('harvestClients');
 var harvest_url = "https://datarunsdeep.harvestapp.com/clients";
 var harvest_options = {
   url: harvest_url,
@@ -16,35 +23,80 @@ var init = function(){
  //get data from harvest
 function getHarvestData(error, response, body) {      	
         if(!error){
-          saveData(body);
+          //console.log(body)
+          removeData(body);
+          //addNew(body)
         }else{
           console.log("error: ", error);
         }
 }
 
-function saveData(body){
-        var clients = JSON.parse(body);
-  sqlQuery = "INSERT INTO drd_harvestClients ( `id`,  `client_name`) VALUES";
-  connection.connect();
+function removeData(body){
+  var clients = JSON.parse(body);
+  sqlQuery = "UPDATE `bigq-drd-1.Timesheets.harvestClients`  SET deleted=TRUE Where id not in ("
+  
   for (var i in clients) {
     if(i >0){
       sqlQuery+=",";
     }
+    sqlQuery+="'"+clients[i].client.id+"'"
     
-    sqlQuery+="("+clients[i].client.id+",'"+config.mysql_real_escape_string (clients[i].client.name)+"') ";
   }
-  sqlQuery +="ON DUPLICATE KEY UPDATE `client_name`=VALUES(`client_name`)";
-  connection.query(sqlQuery, function(err, rows, fields) {
-    connection.end();
-    if (!err) {
-      console.log('The solution is: ', rows);
-    } else{
-      console.log('Error while performing Query.' + err);
+  sqlQuery+=")";
+  bigquery.createQueryStream(sqlQuery)
+  .on('error', console.error)
+  .on('data', function(row) {
+    console(row)
+  })
+  .on('end', function() {
+    console.log("complete")
+    addNew(body);
+  });
+  
+}
+
+function addNew(body){
+  var maxDates = []
+  sqlQuery = "Select max(TIMESTAMP(updated_at)) as updated, max(TIMESTAMP(created_at)) as created from `bigq-drd-1.Timesheets.harvestClients` "
+  bigquery.createQueryStream(sqlQuery)
+  .on('error', console.error)
+  .on('data', function(row) {
+    for(var i in row){
+      maxDates.push(new Date(row[i].value));
+    }
+    
+  })
+  .on('end', function() {
+    var clients = JSON.parse(body);
+    var json=[];
+    for (var i in clients) {
+      var updated = new Date(clients[i].client.updated_at);
+      var created = new Date(clients[i].client.created_at);
+      if(updated>maxDates[0] | created > maxDates[1]){
+        json.push({"id": clients[i].client.id.toString(),
+                  "client_name": clients[i].client.name,
+                  "created_at":clients[i].client.created_at,
+                  "updated_at":clients[i].client.updated_at,
+                  "deleted":false});
+        
+      }
+    }
+    console.log(json)
+    if(json.length > 0 ){
+      csv = json2csv.parse( json, {header:false});
+      fs.writeFile('harvestClient.csv', csv, function(err) {
+        if (err) throw err;
+        console.log('file saved');
+        table.load('harvestClient.csv', function(err, apiResponse) {
+          if (err) throw err;
+          console.log(apiResponse);
+        });
+      });
     }
   });
 }
 
-//init();
+init();
 module.exports = {
   init: init,
 }
