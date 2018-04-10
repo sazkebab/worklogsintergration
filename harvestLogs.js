dc578cf2b480ac33b4e63eb2e3bf51642e156269
Sarah Crooke <sarahcrooke@Sarahs-MBP-2.lan>
2018-04-10 22:40:49 +1000

    finish the big query connection

--- harvestLogs.js
+++ harvestLogs.js
@@ -1,12 +1,15 @@
-var request = require('request');
-var mysql      = require('mysql');
 var config = require('./config');
-var connection = mysql.createConnection(config.databaseOptions);
-var ids;
-var timeData =[];
-var count = 0;
-//SQL table
-harvestLogs_Table = "drd_harvestTimeLogs"
+var request = require('request');
+const json2csv = require('json2csv');
+var fs = require('fs');
+const BigQuery = require('@google-cloud/bigquery');
+//request information
+const bigquery = new BigQuery({
+  projectId: 'bigq-drd-1',
+  keyFilename: 'service-account-file.json'
+});
+const dataset = bigquery.dataset('Timesheets');
+const table = dataset.table('harvestTimeLogs');
 //request form harvest
 var harvestURL_pt1 = "https://datarunsdeep.harvestapp.com/projects/"
 var harvestURL_pt2 = "/entries?from=";
@@ -14,34 +17,44 @@ var harvestURL_pt2 = "/entries?from=";
 var harvest_options = {
   headers: config.harvest 
 };
+var timeData = [];
+var ids;
 //get data from 12 months ago to today of projects which are active
 var init = function(){
+    /*table.load('harvestTimeLogs.csv', function(err, apiResponse) {
+          if (err){
+            console.log(err)
+          }else{
+            console.log(apiResponse);
+          }
+          
+        });*/
     var today = new Date();
     var twelveMonthsAgo = new Date();
     twelveMonthsAgo.setMonth(twelveMonthsAgo.getMonth() - 12);
     td = dateFormat(today);
     sd = dateFormat(twelveMonthsAgo);
     harvestURL_pt2 +=sd+"&to="+td;
-    sqlQuery = "SELECT id from drd_harvestProjects WHERE active = TRUE";
-    connection.connect();
-   connection.query(sqlQuery, function(err, rows, fields) {
-      if (!err) {
-        ids = rows;
+    ids=[];
+    sqlQuery = "Select id from `bigq-drd-1.Timesheets.harvestProjects` WHERE ACTIVE Group By id";
+    bigquery.createQueryStream(sqlQuery)
+      .on('error', console.error)
+      .on('data', function(row) {
+        for(var i in row){
+          ids.push(row[i]);
+        }
         count = 0;
-        getDataFromHarvest();
-  		} else{
-        console.log('Error while performing Query.' + err);
-    	}
-    });
+      })
+      .on('end' ,getDataFromHarvest);
 }
 //request the next project timings
 //if last record then add to SQL
 function getDataFromHarvest(){
   if (count >= ids.length){
       console.log("the end");
-      saveReportData();
+      deleteData();
   }else{
-      harvest_options.url = harvestURL_pt1+ ids[count].id+harvestURL_pt2;
+      harvest_options.url = harvestURL_pt1+ ids[count]+harvestURL_pt2;
       count++;
       request(harvest_options, addDataToArray);
     }  
@@ -51,7 +64,9 @@ function getDataFromHarvest(){
 function addDataToArray(error, response, body) {
   if (!error) {
     //console.log(body);
-    timeData.push(body);
+    if(body.length > 0){
+      timeData.push(body);
+    }
     getDataFromHarvest();     	  
   }else{
    	console.log(error);
@@ -61,49 +76,84 @@ function addDataToArray(error, response, body) {
 
  
 //save data to SQL
-function saveReportData(){
-        sqlQuery_clearAllocations = "DELETE FROM " + harvestLogs_Table;
-       sqlQuery = "INSERT INTO " + harvestLogs_Table + " ( `id`,  `user_id`, `project_id`, `task_id`, `hours`,`date_logged`,`notes`  ) VALUES";
-      var firstInsert = true;
-      for (var i in timeData){
+function deleteData(){
+  console.log(timeData);
+  sqlQuery = "UPDATE `bigq-drd-1.Timesheets.harvestClients`  SET deleted=TRUE Where id not in ("
+  var firstOne = true;
+  for (var i in timeData){
         logs = JSON.parse(timeData[i]);
         for (var j in logs) {
-          //console.log(logs[j]);
-          //if the data had a day entry
-          if( logs[j].day_entry !== undefined &&  logs[j].day_entry !== null){
-            if(firstInsert){
-              firstInsert = false;
-            }else{
-              sqlQuery+=",";
-            }
-            //escape note string
-            notes = logs[j].day_entry.notes === null? "":config.mysql_real_escape_string (logs[j].day_entry.notes.replace('\t','').replace(/(?:\r\n|\r|\n)/g, ' ')).slice(0,160);
-           // notes = notes.replace('\\n',' ');
-            date  = "STR_TO_DATE('"+logs[j].day_entry.spent_at+"','%Y-%m-%d')" ;
-//            console.log(notes);
-            sqlQuery+="("+logs[j].day_entry.id+","+logs[j].day_entry.user_id+","+logs[j].day_entry.project_id+","+logs[j].day_entry.task_id+","+logs[j].day_entry.hours+","+date+",'"+ notes+"') ";
-          }
+          if(!firstOne){
+            sqlQuery+=",";
+           }
+           firstOne = false;
+          sqlQuery+="'"+logs[j].day_entry.id.toString()+"'";
         }
-      }
-      sqlQuery +="ON DUPLICATE KEY UPDATE `project_id`=VALUES(`project_id`), `task_id`=VALUES(`task_id`), `user_id`=VALUES(`user_id`), `hours`=VALUES(`hours`), `date_logged`=VALUES(`date_logged`), `notes`=VALUES(`notes`)";
-    	//delete everything from the table
-     connection.query(sqlQuery_clearAllocations, function(err, rows, fields) {
-        if (!err) {
-            console.log('Cleared Allocations: ', rows);
-            //replace everything in the table
-            connection.query(sqlQuery, function(err, rows, fields) {
-                 connection.end();
-                if (!err) {
-                    console.log('Updated allocations ', rows);
-                }else{
-                    console.log('Error while performing Query.' + err);
-                }
-             });
-        } else{
-            console.log('Error while performing Query.' + err);
-             connection.end();
-        }
+    }
+    sqlQuery+=")";
+   bigquery.createQueryStream(sqlQuery)
+    .on('error', console.error)
+    .on('data', function(row) {
+        console.log(row)
+    })
+    .on('end', function() {
+      console.log("complete");
+      addNew();
     });
+}
+
+
+function addNew(){
+  var maxDates = [];
+  sqlQuery = "Select max(TIMESTAMP(updated_at)) as updated, max(TIMESTAMP(created_at)) as created from `bigq-drd-1.Timesheets.harvestTimeLogs` "
+  bigquery.createQueryStream(sqlQuery)
+  .on('error', console.error)
+  .on('data', function(row) {
+    for(var i in row){
+      if(row[i]!= null){
+        maxDates.push(new Date(row[i].value));
+      }else{
+        maxDates.push(0);
+      }
+    }
+    
+  })
+  .on('end', function() {
+		var json=[];
+		var firstInsert = true;
+    for (var i in timeData){
+        logs = JSON.parse(timeData[i]);
+        for (var j in logs) {
+          var updated = new Date(logs[j].day_entry.updated_at);
+          var created = new Date(logs[j].day_entry.created_at);
+          if(updated>maxDates[0] | created > maxDates[1]){
+            json.push({
+              "id": logs[j].day_entry.id.toString(),
+                  "user_id": logs[j].day_entry.user_id.toString(),
+              "project_id": logs[j].day_entry.project_id.toString(),
+                  "task_id": logs[j].day_entry.task_id.toString(),
+                  "hours": logs[j].day_entry.hours,
+                  "date_logged": logs[j].day_entry.spent_at.toString(),
+                  "notes": logs[j].day_entry.notes === null? "":config.mysql_real_escape_string (logs[j].day_entry.notes.replace('\t','').replace(/(?:\r\n|\r|\n)/g, ' ')).slice(0,160),
+                  "created_at":logs[j].day_entry.created_at,
+                  "updated_at":logs[j].day_entry.updated_at,
+                  "deleted":false});
+        
+				}
+			}
+    }
+    if(json.length > 0 ){
+      csv = json2csv.parse( json, {header:false});
+      fs.writeFile('harvestTimeLogs.csv', csv, function(err) {
+        if (err) throw err;
+        console.log('file saved');
+        table.load('harvestTimeLogs.csv', function(err, apiResponse) {
+          if (err) console.err;
+          console.log(apiResponse);
+        });
+      });
+    }
+  });
 }
 
 function dateFormat(d){
@@ -119,7 +169,7 @@ function dateFormat(d){
     return yyyy.toString()+mm.toString()+dd.toString();
   
 }
-//init();
+init();
 module.exports = {
   init: init,
 }
--- harvestProjectCost.js
+++ harvestProjectCost.js
@@ -1,12 +1,18 @@
 var config = require('./config');
-var mysql      = require('mysql');
-var connection = mysql.createConnection(config.databaseOptions);
 var request = require('request');
+const json2csv = require('json2csv');
+var fs = require('fs');
+const BigQuery = require('@google-cloud/bigquery');
 //request information
+const bigquery = new BigQuery({
+  projectId: 'bigq-drd-1',
+  keyFilename: 'service-account-file.json'
+});
+const dataset = bigquery.dataset('Timesheets');
+const table = dataset.table('harvestProjectCosts');
 var harvest_options = {
   headers: config.harvest 
 };
-//request form harvest
 var harvestURL_pt1 = "https://datarunsdeep.harvestapp.com/projects/"
 var harvestURL_pt2 = "/user_assignments";
 var ids;
@@ -16,18 +22,18 @@ var count = 0;
 	
 //get data from begining of this year to today of projects which are active
 var init = function(){
-    sqlQuery = "SELECT id from drd_harvestProjects WHERE active = TRUE";
-    connection.connect();
-   connection.query(sqlQuery, function(err, rows, fields) {
-      if (!err) {
-        ids = rows;
+	ids=[];
+	sqlQuery = "Select id from `bigq-drd-1.Timesheets.harvestProjects` WHERE ACTIVE Group By id";
+	bigquery.createQueryStream(sqlQuery)
+		.on('error', console.error)
+		.on('data', function(row) {
+			for(var i in row){
+				ids.push(row[i]);
+			}
         count = 0;
-        getDataFromHarvest();
-  		} else{
-        console.log('Error while performing Query.' + err);
-    	}
-    });
- }
+		})
+		.on('end' ,getDataFromHarvest);
+}
  
 
  //save the data to the arrach
@@ -46,39 +52,61 @@ function getDataFromHarvest(){
   if (count >= ids.length){
       saveReportData();
   }else{
-      harvest_options.url = harvestURL_pt1+ ids[count].id+harvestURL_pt2;
+      harvest_options.url = harvestURL_pt1+ ids[count]+harvestURL_pt2;
       count++;
       request(harvest_options, addDataToArray);
     }  
  }
  
 function saveReportData(){
-	 sqlQuery = "INSERT INTO drd_projectcosts ( `project_id`,  `user_id`, `rate`) VALUES";
-      var firstInsert = true;
-      for (var i in costsData){
-        logs = JSON.parse(costsData[i]);
-        for (var j in logs) {
-          if(firstInsert){
-            firstInsert = false;
-          }else{
-            sqlQuery+=",";
-          }
-          sqlQuery+="("+logs[j].user_assignment.project_id+","+logs[j].user_assignment.user_id+","+logs[j].user_assignment.hourly_rate+") ";
-            
-        }
+	 var maxDates = []
+  sqlQuery = "Select max(TIMESTAMP(updated_at)) as updated, max(TIMESTAMP(created_at)) as created from `bigq-drd-1.Timesheets.harvestProjectCosts` "
+  bigquery.createQueryStream(sqlQuery)
+  .on('error', console.error)
+  .on('data', function(row) {
+    for(var i in row){
+      if(row[i]!= null){
+        maxDates.push(new Date(row[i].value));
+      }else{
+        maxDates.push(0);
       }
-      sqlQuery +="ON DUPLICATE KEY UPDATE `rate`=VALUES(`rate`)";
-    	connection.query(sqlQuery, function(err, rows, fields) {
-        connection.end();
-        if (!err) {
-          console.log('The solution is: ', rows);
-        }else{
-          console.log('Error while performing Query.' + err);
-    		}
-    	});
+    }
+    
+  })
+  .on('end', function() {
+		var json=[];
+		var firstInsert = true;
+		for (var i in costsData){
+			logs = JSON.parse(costsData[i]);
+			for (var j in logs) {
+				var updated = new Date(logs[j].user_assignment.updated_at);
+				var created = new Date(logs[j].user_assignment.created_at);
+				if(updated>maxDates[0] | created > maxDates[1]){
+					json.push({"project_id": logs[j].user_assignment.project_id.toString(),
+                  "user_id": logs[j].user_assignment.user_id.toString(),
+                  "rate": logs[j].user_assignment.hourly_rate,
+                  "created_at":logs[j].user_assignment.created_at,
+                  "updated_at":logs[j].user_assignment.updated_at,
+                  "deleted":false});
+        
+				}
+			}
+		}
+		if(json.length > 0 ){
+      csv = json2csv.parse( json, {header:false});
+      fs.writeFile('harvestProjectCosts.csv', csv, function(err) {
+        if (err) throw err;
+        console.log('file saved');
+        table.load('harvestProjectCosts.csv', function(err, apiResponse) {
+          if (err) throw err;
+          console.log(apiResponse);
+        });
+      });
+    }
+	});
 }
 
-//init();
+init();
 module.exports = {
   init: init,
 }
